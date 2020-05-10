57f325354a124210fd6606ee0d0de5051e349257
Milvio LuA <milviolua@gmail.com>
2020-05-10 18:56:30 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function generate() {
+ var name = document.getElementById("nameinput").value;
+ var age = document.getElementById("ageinput").value;
+
+ var username = name + " " + age;
+ alert(username);
+
+}
